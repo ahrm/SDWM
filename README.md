35b282b3fee001694ebe188f5509c840e5b9d2f4
ahrm <a.hr.mostafavi@gmail.com>
2022-07-28 01:25:25 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple Dynamic Window Manager for Windows
+
+When I am working on a computer 99% of my interaction is using one of the following window configurations:
+- A single fullscreen window (e.g. when I am coding, browsing the web, etc.)
+- Two equal windows side-by-side (e.g. when debugging or when coding while reading documentation)
+
+So the normal window managers waste a lot of bandwidth making all sorts of (to me) useless window configurations possible, this is an issue which tiling window managers tend to fix. On linux I normally use `i3` which is a decent (though not perfect) tiling window manager. The main feature that I miss in `i3` is the ability to have the same program on multiple workspaces, which is pretty useful for my use cases. For example sometimes I want to have a code editor and a browser side-by-side when reading documentation in a documentation workspace and once I am done with the documentation I want to move to a workspace with fullscreen code editor. (I am sure there is an extension or script that makes that possible, but at least it is not a first-class feature of `i3`). `dwm` is another window manager that does have a feature similar to this, however, as much as I enjoy being stuck in a boot-loop because I made a mistake in a "config" file, sometimes I need to actually get work done on my computer which seems to be the antithesis to `dwm`'s philosophy.
+
+I have tried many tiling window managers on windows over the years, however, because windows's window manager is not really designed to be customized they all eventually led to frustrations that made me give up on them.
+
+In this repository I try to create an *extremely* simple window manager for windows that tries to optimize my own use cases. The main idea is not to mess with window's window management as much as possible. Here is how it works: Whenever you run `window-manager.py`, it takes a snapshot of all open windows at the instance that you ran it and creates a dummy window. Whenever you focus on this window, instead of focusing the window, it restores all windows to the position and size that they were when the snapshot were taken. So for example for my use-case of having two configurations, one for reading documentation while coding and the other exclusively for coding, I can first create a snapshot when my code editor is full screen and then create another snapshot when editor and browser are snapped to different sides of the screen and toggle between these two configurations by focusing on these "dummy" windows. This method does not mess with windows' window management system at all and the system's behavior is completely indistinguishable from normal behavior except for when you focus on one of these dummy windows. Another advantage is that these windows can be controlled naturally using windows's normal window management shortcuts (e.g. control+tab) so your muscle memory is applicable.
+
+Here is my setup:
+* I use [autohotkey](https://www.autohotkey.com/) to lauch `window-manager.py` (I use `Win+Space` as the shortcut). Here is the script that I use:
+```
+#Space::
+run pythonw C:\path\to\window-manager.py
+return
+```
+* There is a textbox when you run `window-manager.py` where you can optionally enter a name for the dummy window. I use this name along with [switcheroo](https://github.com/kvakulo/Switcheroo) to quickly switch to a workspace by typing its name.
---
+++ window-manager.py
@@ -0,0 +1,118 @@
+import os
+import datetime
+import time
+import ctypes
+user32 = ctypes.windll.user32
+from PIL.ImageQt import ImageQt
+import PIL.ImageGrab
+import PyQt5
+import PyQt5.QtWidgets
+import PyQt5.QtGui
+import win32con
+import win32gui
+
+NAME = 'SDWM'
+
+def restore_state(state, foreground_window):
+	prev = None
+	for i, (hwnd, placement) in enumerate(reversed(state)):
+		name = win32gui.GetWindowText(hwnd)
+		try:
+			win32gui.SetWindowPlacement(hwnd, placement)
+			rect = placement[-1]
+			win32gui.SetWindowPlacement(hwnd, placement)
+			if i < len(state) - 1:
+				win32gui.SetWindowPos(hwnd, state[-(i+2)][0], rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1], win32con.SWP_SHOWWINDOW)
+			else:
+				win32gui.SetWindowPos(hwnd, win32con.HWND_TOP, rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1], win32con.SWP_SHOWWINDOW)
+
+			win32gui.SetForegroundWindow(hwnd)
+			# this should not be necessary but for some reason if we remove this, windows sometimes
+			# confuses the order of windows, I am not sure if this value works for all systems, you
+			# might need to increase it if you see inconsistent behavior
+			time.sleep(0.04)
+		except:
+			pass
+	try:
+		win32gui.SetForegroundWindow(foreground_window)
+	except:
+		pass
+
+def get_window_order_helper(hwnd):
+	if not hwnd:
+		return []
+	next_window = user32.GetWindow(hwnd, win32con.GW_HWNDNEXT)
+	if win32gui.IsWindowVisible(hwnd) and len(win32gui.GetWindowText(hwnd)) > 0:
+		return [hwnd] + get_window_order_helper(next_window)
+	else:
+		return get_window_order_helper(next_window)
+	
+def get_visible_window_order():
+	top = user32.GetTopWindow(None)
+	return get_window_order_helper(top)
+
+def get_window_placement(hwnd):
+	placement = win32gui.GetWindowPlacement(hwnd)
+	rect = win32gui.GetWindowRect(hwnd)
+	processed_placement = (placement[0], placement[1], placement[2], placement[3], rect)
+	return processed_placement
+
+if __name__ == '__main__':
+	icon_path = os.path.dirname(os.path.abspath(__file__)) + r'\SDWM-icon.png'
+
+	active = False
+	windows = get_visible_window_order()
+	window_placements = list(map(get_window_placement, windows))
+	foreground_window = win32gui.GetForegroundWindow()
+	state = list(zip(windows, window_placements))
+	screenshot = PIL.ImageGrab.grab()
+	last_focus_time = datetime.datetime.now()
+
+	def on_focus(first, second):
+		if active:
+			if first == None:
+				restore_state(state, foreground_window)
+				last_focus_time = datetime.datetime.now()
+
+	app = PyQt5.QtWidgets.QApplication([])
+	window = PyQt5.QtWidgets.QWidget()
+	layout = PyQt5.QtWidgets.QVBoxLayout()
+	pic = PyQt5.QtWidgets.QLabel()
+	pic.setPixmap(PyQt5.QtGui.QPixmap.fromImage(ImageQt(screenshot).copy()))
+	name = PyQt5.QtWidgets.QLineEdit()
+
+	# set window icon
+	icon = PyQt5.QtGui.QIcon(icon_path)
+	app.setWindowIcon(icon)
+
+	def on_return_pressed():
+		global active
+		active = True
+		if len(name.text()) > 0:
+			window.setWindowTitle(NAME +  ' > ' + name.text())
+		else:
+			window.setWindowTitle(NAME)
+		window.showMinimized()
+
+	def on_timout():
+		global active
+		if active:
+			if (datetime.datetime.now() - last_focus_time).seconds > 1:
+				window.showMinimized()
+
+	# if we minimize it in focusEvent, then the window is not properly rendered
+	# so we try to minimize it periodically here
+	# (if we don't minimize the window at all, then the user might accidentally activate
+	# the window by minimizing other windows)
+	timer = PyQt5.QtCore.QTimer()
+	timer.timeout.connect(on_timout)
+	timer.start(1000)
+
+	name.returnPressed.connect(on_return_pressed)
+	layout.addWidget(name)
+	layout.addWidget(pic)
+	window.setLayout(layout)
+	window.setFocusPolicy(PyQt5.QtCore.Qt.StrongFocus)
+	window.showMaximized()
+	app.focusChanged.connect(on_focus)
+	app.exec()
